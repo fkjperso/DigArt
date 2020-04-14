9663b2a4c9b5ab457a872c2fd7d745b9dc25f2a6
fkjperso <63605272+fkjperso@users.noreply.github.com>
2020-04-14 12:43:28 +0200

    Add files via upload

--- art_gan6.py
+++ art_gan6.py
@@ -101,13 +101,13 @@ def save_images(cnt, noise):
             c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
             image_array[r:r + IMAGE_SIZE, c:c +
                         IMAGE_SIZE] = generated_images[image_count] * 255
-            image_count += 1
-            output_path = 'output'
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-                filename = os.path.join(output_path, f"trained-{cnt}.png")
-                im = Image.fromarray(image_array)
-                im.save(filename)
+    image_count += 1
+    output_path = 'output'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    filename = os.path.join(output_path, f"trained-{cnt}.png")
+    im = Image.fromarray(image_array)
+    im.save(filename)
 
 
 
